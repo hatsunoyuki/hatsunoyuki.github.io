bbc5f1cd0b6e071f097aaa8b9f7c43d18ab47c92
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2020-04-11 16:54:08 +0800

    Update 0411main.js

--- myfile/0411main.js
+++ myfile/0411main.js
@@ -1,3 +1,3 @@
 // $(".title").before()
 // $(document).ready(function () {$("#wenbenkuang").prev().before('')});
-$("#contentdiv").prepend('<script src="https://hatsunoyuki.github.io/myfile/0411test.js"></script><link rel="stylesheet" href="https://hatsunoyuki.github.io/myfile/0411.css">}</style>')
+$("#contentdiv").prepend('<script src="https://hatsunoyuki.github.io/myfile/0411test.js"></script><link rel="stylesheet" href="https://hatsunoyuki.github.io/myfile/0411.css">')
