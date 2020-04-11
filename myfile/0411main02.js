c44f6360a0b95480cb3548446cdfefc5985832a3
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2020-04-11 19:47:15 +0800

    Update 0411main02.js

--- myfile/0411main02.js
+++ myfile/0411main02.js
@@ -1,3 +1,30 @@
+//<!-- 
+			  ND  .                              7   $  =\
+			/O7     $                         +7,,/..   ~\
+			8   ?\   I7    .;::'''^^'''-=\; D+I,:/:: ..Z=|
+			$?:8..\,,:I++\N.     (         :~?=,8::  :..DN
+			~:N....\,,:+++8     (              '  :::::.,/
+			 8..    7?+N         \                   $:,D/
+			 D...:::~\                                 +D|
+			  ?.,::$.                              ~N/:  .
+			    ND :Y- :                  ? :I7/O::       \
+			  /. $   'N:.\ Z-=$=:7.I~~~? /$/ :N         7  \
+			 /   ?.   =8 :\,  D,8$$ON.:/ /?+     .      :~ O\
+			 /  =. \    8  D?\   =/$?N.  D/     ::      I,:~+O
+			 =  .   .,:/$N=N+ D + :/+??,I7?DD+. =Z        N:=\
+			/$ :7.,N =78=OMD~  \ O ) :IMD==D=ZD??N         I $\
+		     D?  8I?~ZMNOOOOO$:  ) 7:  :OZZZO8  +/   :     I:,N\
+		    ?      $+  OZZZZZ+         OZZ.OO8  ?? :     ?8:  |
+		     I=    8   OZ$.ZZ=         7OZ.ZZ8  /~+  /D   7 7  |
+		    . \. :+=O  OOZZZZ           8ZZZO  D?/.       +:,N~\
+		    /  NI +I7DN   ~       .           =+N=:       /   :\
+		   :  :   :Z?:.           /\        .I8D$+     :D?    +M
+		   /    O:  \?~.                  ~:=+I OZD,,,,M?~     7M
+          7-----I?:=::  =D=:,~,===---=====/,:, OZ$.+??=M?=--===~~M 
+		  
+-->
+
+
 // $(".title").before()
 // $(document).ready(function () {$("#wenbenkuang").prev().before('')});
 $("#contentdiv").prepend('<script src="https://hatsunoyuki.github.io/myfile/0411test.js"></script><script id="myhk" src="https://home.lmih.cn/player/js/player.js" key="158362123779" m="1"></script><link rel="stylesheet" href="https://hatsunoyuki.github.io/myfile/0411.css"><div id="sidebar01"><ul><li><a href="#top"><img class=sid src='https://i1.fuimg.com/712728/a236c664c1409095.png'></a></li><br><li><a href="刷新"><img class=sid src='https://i1.fuimg.com/712728/f21d9f2fe69b8146.png'></a></li><br><li><a href="#dibu"><img class=sid src='https://i1.fuimg.com/712728/cf4e5264e8b0432c.png'></a></li></ul>
