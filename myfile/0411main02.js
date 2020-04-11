1f2c78ddb994fbc9251b4729696450dd8806ee0c
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2020-04-11 20:13:20 +0800

    Update 0411main02.js

--- myfile/0411main02.js
+++ myfile/0411main02.js
@@ -1,8 +1,9 @@
 /* .. 
+                                                              . ':
                          ND  .                              7   $  =\
                        /O7     $                         +7,,/..   ~\
-                       8   ?\   I7    .;::'''^^'''-=\; D+I,:/:: ..Z=|
-                       $?:8..\,,:I++\N.     (         :~?=,8::  :..DN
+                       (   ?\   I7.   .;::'''^^'''-=\; D+I,:/:: ..Z=|
+                       $ :8..\,,:I++\N.     (         :~?=,8::  :..DN
                        ~:N....\,,:+++8     (              '  :::::.,/
                         8..    7?+N         \                   $:,D/
                         D...:::~\                                 +D|
@@ -18,8 +19,8 @@
                        I=    8   OZ$.ZZ=          7OZ.ZZ8  /~+  /D   7 7  |
                       . \. :+=O  OOZZZZ            8ZZZO  D?/.       +:,N~\
                       /  NI +I7DN   ~        .           =+N=:       /   :\
-                     :  :   :Z?:.            /\        .I8D$+     :D?    +M
-                     /    O:  \?~.                   ~:=+I OZD,,,,M?~     7M
+                     :  :   :Z?:.            /\        .I8D$+     :D?    +|
+                     /    O:  \?~.                   ~:=+I OZD,,,,M?~     7|
                     7-----I?:=::  =D=:,~,===---=====/,:, OZ$.+??=M?=--===~~M 
 
 天惹噜,你发现了一只野生的雪雪!
@@ -31,6 +32,12 @@
 
 // $(".title").before()
 // $(document).ready(function () {$("#wenbenkuang").prev().before('')});
-$("#contentdiv").prepend('<script src="https://hatsunoyuki.github.io/myfile/0411test.js"></script><script id="myhk" src="https://home.lmih.cn/player/js/player.js" key="158362123779" m="1"></script><link rel="stylesheet" href="https://hatsunoyuki.github.io/myfile/0411.css"><div id="sidebar01"><ul><li><a href="#top"><img class=sid src='https://i1.fuimg.com/712728/a236c664c1409095.png'></a></li><br><li><a href="刷新"><img class=sid src='https://i1.fuimg.com/712728/f21d9f2fe69b8146.png'></a></li><br><li><a href="#dibu"><img class=sid src='https://i1.fuimg.com/712728/cf4e5264e8b0432c.png'></a></li></ul>
-</div>')
+$("#contentdiv").prepend('<script src="https://hatsunoyuki.github.io/myfile/0411test.js"></script>
+                         <script id="myhk" src="https://home.lmih.cn/player/js/player.js" key="158362123779" m="1"></script>
+                         <link rel="stylesheet" href="https://hatsunoyuki.github.io/myfile/0411.css">
+                         <div id="sidebar01"><ul><li><a href="#top">
+                         <img class=sid src='https://i1.fuimg.com/712728/a236c664c1409095.png'></a>
+                        </li><br><li><a href="刷新"><img class=sid src='https://i1.fuimg.com/712728/f21d9f2fe69b8146.png'></a></li>
+                         <br><li><a href="#dibu"><img class=sid src='https://i1.fuimg.com/712728/cf4e5264e8b0432c.png'></a></li></ul>
+                        </div>')
                          
