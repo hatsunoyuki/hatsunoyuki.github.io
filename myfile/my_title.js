e08b686ad34e2bedf137d6fb490a94468b6daeea
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2020-04-17 23:13:59 +0800

    Update my_title.js

--- myfile/my_title.js
+++ myfile/my_title.js
@@ -17,13 +17,3 @@ var scrWidth = window.screen.width;  /* 标题icon */
             headHTML    += '<link rel="shortcut icon" href="https://i2.tiimg.com/712728/0b307add2a7b5e71.png" type="image/x-icon">';
 			document.getElementsByTagName('head')[0].innerHTML = headHTML;
 
-$(document).ready(function() {/* 鼠标样式 */
-	$('body').hover(
-        function() {
-            $(this).css({
-                cursor: "url("https://hatsunoyuki.github.io/myfile/mycursor.ani"),auto"
-                    })
-                },
-       )
-    })
-
