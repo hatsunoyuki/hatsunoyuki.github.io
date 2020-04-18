7d4ac85f817f9e097fbb2852c1a4148669b5b1bf
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2020-04-18 13:01:43 +0800

    Update test01.js

--- mytest/test01.js
+++ mytest/test01.js
@@ -5,9 +5,7 @@ $(document).ready(function () {
     add.setAttribute("id", "yeDocarateDiv");
     select.before(add);
     add.innerHTML =
-        '<a href="javascript:;" Onclick="$(&apos;#yeColor&apos;).toggle()">测试1</a><br> <form id="yeColor" style="display: none;">测试2<a href="https://kuler.adobe.com" target="_blank">鑹插僵鎼厤鍙傝€冪偣鎴�</a><br>閫夋嫨棰滆壊锛� <input type="color" id="ye_color"><p>   <select id="ye_decorate_select"><option >閫夋嫨闇€瑕佹洿鏀圭殑鏍峰紡</option>     <option value="ye_whole">鐣岄潰鏁翠綋鍙樿壊</option>          <option value="ye_banner_shadow">banner闃村奖棰滆壊</option>     <option value="ye_reply_font">鍥炲鏂囧瓧棰滆壊</option><br>     <option value="ye_name_time">鏄电О琛岄鑹�</option>     <option value="ye_block">鍒嗛殧绾块鑹�</option><br>     </select>  <button id="ye_decorate" type="button">棰勮</button><button id="ye_save_decorate" type="button">淇濆瓨</button></p> </form><a href="javascript:;" Onclick="$(&apos;#yeTalk&apos;).toggle()">灏忓彾蹇嵎瀵硅瘽</a><br><p id="yeTalk" style="display: none;">閫夋嫨灏嗗唴瀹瑰彂閫佸埌甯栧瓙涓椂浼氬嚭鐜颁竴瀹氬欢杩熴€�'+yeTalkSelect+'</p><div id="show_zcxiaoye" onclick="judgezcXiaoye();">鏄剧ず鎴栭殣钘忔瀹犲皬鍙�</div><div id="show_xiaoye" onclick="judgeXiaoye();">閫夋嫨鏄剧ず鎶曟幏灏忓彾鎴栨憞鎽囧彾</div>';
-    $("#ye_decorate").click(decorateYeUnSave);
-    $("#ye_save_decorate").click(decorateYe);
+        '<div id="aa" style="display: none;">测试2</div>';
 
 
 });
