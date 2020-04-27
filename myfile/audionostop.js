d3f9f07523f64e8724bc63fd5f177682292c334b
hatsunoyuki <blove36577@gmail.com>
2020-04-27 18:45:49 +0800

    update

--- myfile/audionostop.js
+++ myfile/audionostop.js
@@ -1,15 +1,29 @@
-$(document).pjax('a[target!=_blank]', '#app', {fragment: '#app',timeout: 8000}); //#wrap为刷新的id
-    $(document).on('pjax:send', function() {
-    $(".loading").css("display", "block");
-    //预加载函数可写在这里
+$(document).pjax("#yerefresh", ".reply_list", {
+    fragment: ".reply_list",
+    timeout: 9000,
+    scrollTo: "#dibu"
 });
 
-$(document).on('pjax:complete', function() {
-    //回调函数
-    $("img[src$=jpg],img[src$=jpeg],img[src$=png],img[src$=gif]").parent("a[class!=noview]").addClass("swipebox");
-    if( $('pre').length ){ prettyPrint(); }     //回调函数解决文章页代码不高亮的问题
-    $(".loading").css("display", "none");
-    //pjax加载结束的回调函数 解决js无法定位的问题
-    //重新定位容器内容的函数写在这里
+$(document).on("submit", "form", function(event) {
+    $.pjax.submit(event, ".reply_list", {
+        fragment: ".reply_list",
+        timeout: 9000,
+        push: false,
+        scrollTo: "#dibu"
+    });
 });
-
+$(document).on("click", "center>div a", function(event) {
+    var url = $(this).attr("href");
+    $.pjax.reload("#app>center", {
+        fragment: "center",
+        timeout: 9000,
+        url: "https://nmmnm.co/showmsg/forumid/22/id/404772",
+        async: false
+    });
+    $.pjax.reload(".reply_list", {
+        fragment: ".reply_list",
+        timeout: 9000,
+        url: "https://nmmnm.co/showmsg/forumid/22/id/404772",
+    });
+    event.preventDefault();
+});
