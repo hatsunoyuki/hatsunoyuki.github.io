6b98ef808428f2853e9e84206febe01a4e5fd743
hatsunoyuki <blove36577@gmail.com>
2020-04-27 18:27:00 +0800

    update

--- myfile/audionostop.js
+++ myfile/audionostop.js
@@ -1,43 +1,15 @@
-$(document).pjax("#refresh", ".reply_list", {
-    fragment: ".reply_list",
-    timeout: 9000,
-    scrollTo: "#dibu"
+$(document).pjax('a[target!=_blank]', '#app', {fragment: '#app',timeout: 8000}); //#wrap为刷新的id
+    $(document).on('pjax:send', function() {
+    $(".loading").css("display", "block");
+    //预加载函数可写在这里
 });
 
-// $(document).pjax("center > div a", ".reply_list", {
-//   fragment: ".reply_list",
-//   timeout: 9000, async: false
-// });
-// $(document).pjax("center > div a", "center", {
-//     fragment: "center",
-//     timeout: 9000
-//   });
-$(document).on("submit", "form", function(event) {
-    $.pjax.submit(event, ".reply_list", {
-        fragment: ".reply_list",
-        timeout: 9000,
-        push: false,
-        scrollTo: "#dibu"
-    });
+$(document).on('pjax:complete', function() {
+    //回调函数
+    $("img[src$=jpg],img[src$=jpeg],img[src$=png],img[src$=gif]").parent("a[class!=noview]").addClass("swipebox");
+    if( $('pre').length ){ prettyPrint(); }     //回调函数解决文章页代码不高亮的问题
+    $(".loading").css("display", "none");
+    //pjax加载结束的回调函数 解决js无法定位的问题
+    //重新定位容器内容的函数写在这里
 });
-$(document).on("click", "center>div a", function(event) {
-    var url = $(this).attr("href");
-    $.pjax.reload("#app>center", {
-        fragment: "center",
-        timeout: 9000,
-        url: url,
-        async: false
-    });
-    $.pjax.reload(".reply_list", {
-        fragment: ".reply_list",
-        timeout: 9000,
-        url: url
-    });
-    event.preventDefault();
-});
-// $("center>div a").click(function(event) {
-//   var url = $(this).attr("href");
-//   $.pjax.reload("#app>center", { fragment: "center",timeout: 9000, url: url, async: false });
-//   $.pjax.reload(".reply_list", { fragment: ".reply_list", timeout: 9000,url: url });
-//   event.preventDefault();
-// });
+
