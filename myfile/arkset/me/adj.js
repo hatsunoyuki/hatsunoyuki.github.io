f87c6904ee104ba90fa340011bc50199e6444f08
hatsunoyuki <blove36577@gmail.com>
2020-06-24 20:51:51 +0800

    ud

---
+++ myfile/arkset/me/adj.js
@@ -0,0 +1,33 @@
+document.title='H岛模拟测试demo';
+
+window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-100439904-7');
+
+
+$(".container")[1].id='fstcon';
+
+$(".topic").before("<br>");
+
+$("#keep_status").append("<div class='titleme'><p>★★★★★★</p></div>");
+
+setInterval("jnkc.innerHTML=new Date().toLocaleString()+' 星期'+'日一二三四五六'.charAt(new Date().getDay());",1000);
+
+
+						
+var fontCfg = {
+    p:'R8hVguNKZlfMuPoP',
+        fonts: [
+            {
+                fontName: 'FZTYSJW' ,
+                selecters: ['#li01','#li02','#li03']
+            }
+                ]
+                    };
+var myFont = new FTFont(fontCfg);
+
