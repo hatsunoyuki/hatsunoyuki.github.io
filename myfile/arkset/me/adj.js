309ba3db0d90eba12ea3ba7899c5d882c339f0b6
hatsunoyuki <blove36577@gmail.com>
2020-06-25 16:55:44 +0800

    Update adj.js

--- myfile/arkset/me/adj.js
+++ myfile/arkset/me/adj.js
@@ -1,5 +1,15 @@
 document.title='H岛模拟测试demo';
 
+ var bodyBgs = [];
+        bodyBgs[0] = "https://s1.ax1x.com/2020/06/24/Nw1zM8.png";
+        bodyBgs[1] = "https://s1.ax1x.com/2020/06/25/NBQrSP.png";
+		bodyBgs[2] = "https://s1.ax1x.com/2020/06/25/NBlSl6.png";
+
+
+        var randomBgIndex = Math.round( Math.random() * 2 );
+
+    //输出随机的背景图
+        document.write('<style>.themenu{background:url(' + bodyBgs[randomBgIndex] + ') no-repeat}</style>');
 
 window.onload=function(){ 
 setInterval(function(){  
@@ -20,12 +30,4 @@ $(".topic").before("<br>");
 $("#keep_status").append("<div class='titleme'><p>★★★★★★</p></div>");
 
 
- var bodyBgs = [];
-        bodyBgs[0] = "https://s1.ax1x.com/2020/06/24/Nw1zM8.png";
-        bodyBgs[1] = "https://s1.ax1x.com/2020/05/14/YBzQ4x.jpg";
 
-
-        var randomBgIndex = Math.round( Math.random() * 1 );
-
-    //输出随机的背景图
-        document.write('<style>.themenu{background:url(' + bodyBgs[randomBgIndex] + ') no-repeat}</style>');
