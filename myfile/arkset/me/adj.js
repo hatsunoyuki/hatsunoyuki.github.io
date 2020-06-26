df774d8f25bb5408fe01942694d3236df1b1286f
hatsunoyuki <blove36577@gmail.com>
2020-06-26 23:59:10 +0800

    ud

--- myfile/arkset/me/adj.js
+++ myfile/arkset/me/adj.js
@@ -28,17 +28,6 @@ function tips(){
 }
 
 
-window.onload=function(){ 
-setInterval(function(){  
-var date=new Date();  
-var year=date.getFullYear(); //获取当前年份  
-var mon=date.getMonth()+1; //获取当前月份  
-var da=date.getDate(); //获取当前日  
-var h=date.getHours(); //获取小时  
-var m=date.getMinutes(); //获取分钟  
-var d=document.getElementById('time');  
-d.innerHTML=''+year+'/'+mon+'/'+da+' '+h+':'+m; },1000) }
-
 
 $(".container")[1].id='fstcon';
 
