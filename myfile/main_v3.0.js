79863756bbd3aa0fa95a20f76502de7e0b9a441c
hatsunoyuki <blove36577@gmail.com>
2020-07-08 17:10:08 +0800

    new

---
+++ myfile/main_v3.0.js
@@ -0,0 +1,24 @@
+
+    function browserRedirect() {  
+    var sUserAgent = navigator.userAgent.toLowerCase();  
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";  
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";  
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";  
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";  
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";  
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";  
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";  
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";  
+    // document.writeln("您的浏览设备为：");  
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {  
+      
+    // alert("手机浏览！");  
+      
+    document.write('<script type="text/javascript" charset="utf-8" src="https://hatsunoyuki.github.io/myfile/main_v2.6beta.js"><\/script>');  
+    } else {  
+      
+    // alert("PC浏览！");  
+    document.write('<script type="text/javascript" charset="utf-8" src="test01.js"><\/script>');  
+    }  
+    }  
+    browserRedirect();  
