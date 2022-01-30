aefb871d79096120c2f8a2733d521cbfd50bfe54
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2022-01-31 01:21:26 +0800

    Update sl_emotion_v4.0.js

--- myfile/3.0/sl_emotion_v4.0.js
+++ myfile/3.0/sl_emotion_v4.0.js
@@ -26,58 +26,52 @@ $(document).ready(function () {
         $("textarea:last()").val($("textarea:last()").val() + '<textarea class="form-control"></textarea>');
     });	
     $("#snow02").on("click", function() {
-      var imglist = [
+      if($("#snow01").is(":hidden")){
+       $("#snow01").show();
+      $("#snow01").empty();/* 只出现一次 */
 
-
-      ];
-      $("#snow01").empty();
-      for (var b = 0; b < imglist.length; b++) {
-        var snowemo =
-          "<img src='" +
-          imglist[b] +
-          "' class='emot-class' Onclick='addemot(\"" +
-          imglist[b] +
-          "\")'>";
-        $("#snow01").append(snowemo);
-      }
       $("#snow01").append(
         '<font id="shanchu" color="#4682B4">删除</font>&nbsp;&nbsp;<font id="zhedie" color="#4682B4">折叠</font>&nbsp;&nbsp;<font id="lianjie" color="#4682B4">点就看</font>&nbsp;&nbsp;<font id="fanbai" color="#4682B4">反白</font>&nbsp;&nbsp;<font id="wenben" color="#4682B4">文本框</font>'
       );
+	  
+	  
       $("#shanchu").on("click", function() {
         $("textarea:last()").val(
           $("textarea:last()").val() +
             '<s></s>'
         );
-        $(".am-tabs-bd").hide();
+        $("#snow01").hide();
       });
       $("#zhedie").on("click", function() {
         $("textarea:last()").val(
           $("textarea:last()").val() +
             '<details><summary>Click to open</summary>内容</details>'
         );
-        $(".am-tabs-bd").hide();
+        $("#snow01").hide();
       });
       $("#lianjie").on("click", function() {
         $("textarea:last()").val(
           $("textarea:last()").val() +
             '<a href="地址" target="_blank">点就看</a>'
         );
-        $(".am-tabs-bd").hide();
+        $("#snow01").hide();
       });
       $("#fanbai").on("click", function() {
         $("textarea:last()").val(
           $("textarea:last()").val() +
             '<font id="fbai"></font>'
         );
-        $(".am-tabs-bd").hide();
+        $("#snow01").hide();
       });        
       $("#wenben").on("click", function() {
         $("textarea:last()").val(
           $("textarea:last()").val() +
             '<textarea class="form-control"></textarea>'
         );
-        $(".am-tabs-bd").hide();
-      });	  
-      
-    });
+        $("#snow01").hide();
+      });	
+	}else{
+      $("#snow01").hide();     //如果元素为显现,则将其隐藏
+	};	  
+    });    
 });
