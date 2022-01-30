df97d75287eea04ac1118f052f9580c12090769b
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2022-01-31 01:30:51 +0800

    Update sl_emotion_v4.0.js

--- myfile/3.0/sl_emotion_v4.0.js
+++ myfile/3.0/sl_emotion_v4.0.js
@@ -8,8 +8,9 @@ $(document).ready(function () {
 		
 	$("div.title").before("<div align='center' id='snow'></div>");
     $("#snow").html(snow_lp);
-    $(".emotion_tab:last()").after('<div id="snow01"></div>');
+    $(".emotion_tab:last()").after('<div id="snow01" style="display:none"></div>');
     $(".emotion_tab:last()").after("<li id='snow02' class='emotion_tab'>豹豹抱抱</li>");
+	
     $("#fanbai").on("click", function () {
         $("textarea:last()").val($("textarea:last()").val() + '<font id="fbai"></font>');
     });
@@ -25,6 +26,7 @@ $(document).ready(function () {
     $("#wenben").on("click", function () {
         $("textarea:last()").val($("textarea:last()").val() + '<textarea class="form-control"></textarea>');
     });	
+	
     $("#snow02").on("click", function() {
       if($("#snow01").is(":hidden")){
        $("#snow01").show();
@@ -32,8 +34,7 @@ $(document).ready(function () {
 
       $("#snow01").append(
         '<font id="shanchu" color="#4682B4">删除</font>&nbsp;&nbsp;<font id="zhedie" color="#4682B4">折叠</font>&nbsp;&nbsp;<font id="lianjie" color="#4682B4">点就看</font>&nbsp;&nbsp;<font id="fanbai" color="#4682B4">反白</font>&nbsp;&nbsp;<font id="wenben" color="#4682B4">文本框</font>'
-      );
-	  
+      );	  
 	  
       $("#shanchu").on("click", function() {
         $("textarea:last()").val(
