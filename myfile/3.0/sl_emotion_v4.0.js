ae59193f93b81c5f3b9aa058005e89639c960540
hatsunoyuki <62078443+hatsunoyuki@users.noreply.github.com>
2022-01-31 21:00:38 +0800

    up

--- myfile/3.0/sl_emotion_v4.0.js
+++ myfile/3.0/sl_emotion_v4.0.js
@@ -1,12 +1,9 @@
-window.onload=function(){
-	$(".text-success").parents("div.reply_box").css("display","none");/* 隐藏已删除的楼层 */
-}
-
+	
 var snow_lp = "";
 var snowinner=document.createElement("small");
 snowinner.innerHTML='<font id="shanchu" color="#4682B4">删除</font>&nbsp;&nbsp;<font id="zhedie" color="#4682B4">折叠</font>&nbsp;&nbsp;<font id="lianjie" color="#4682B4">点就看</font>&nbsp;&nbsp;<font id="fanbai" color="#4682B4">反白</font>&nbsp;&nbsp;<font id="wenben" color="#4682B4">文本框</font>';      
 $(document).ready(function () {
-		
+	$(".text-success").parents("div.reply_box").css("display","none");/* 隐藏已删除的楼层 */	
 	$("div.title").before("<div align='center' id='snow'></div>");
     $("#snow").html(snow_lp);
     $(".emotion_tab:last()").after('<div id="snow01" style="display:none"></div>');
