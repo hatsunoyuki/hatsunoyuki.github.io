8b2a31a0edf1c8c00797f233a10e24568fe88ca8
Your Name <you@example.com>
2022-02-09 03:52:48 +0800

    sum

--- myfile/3.0/sl_emotion_v4.0.js
+++ myfile/3.0/sl_emotion_v4.0.js
@@ -58,3 +58,55 @@ $(document).ready(function () {
 	
       
 });
+
+$(".foot_btn_group >.left").prepend('<input type="button" class="nickname" value="嘘……">');
+
+var replaceArry = [
+		[/芙芙/gi, '可莉'],
+        [/茶茶/gi, '魈宝'],
+        [/茜茜/gi, '贝贝'],
+        [/琪琪/gi, '甘雨'],
+        [/塔塔/gi, '旅行者'],
+        [/冥冥/gi, '胡桃'],
+        [/曰曰/gi, '胡桃'],
+        [/娜娜/gi, '七七'],
+        [/某某/gi, '钟离'],
+        [/妮妮/gi, '刻晴'],
+        [/微微/gi, '温迪'],
+        [/菲菲/gi, '烟绯'],
+        [/e哥/gi, '空哥'],
+        [/薇薇/gi, '温温'],
+	];
+
+$(".nickname").click(
+		function swapname() {
+		var numTerms = replaceArry.length;
+		var txtWalker = document.createTreeWalker(
+			document.body,
+			NodeFilter.SHOW_TEXT, {
+				acceptNode: function(node) {
+					if(node.nodeValue.trim())
+						return NodeFilter.FILTER_ACCEPT;
+
+					return NodeFilter.FILTER_SKIP;
+				}
+			},
+			false
+		);
+		var txtNode = null;
+
+
+		while(txtNode = txtWalker.nextNode()) {
+			var oldTxt = txtNode.nodeValue;
+
+			for(var J = 0; J < numTerms; J++) {
+				oldTxt = oldTxt.replace(replaceArry[J][0], replaceArry[J][1]);
+			}
+			txtNode.nodeValue = oldTxt;
+		}
+	
+		setTimeout(swapname, 1000);
+		setInterval(function() {
+			document.addEventListener("scroll", swapname, true);
+		}, 3000);
+	});
